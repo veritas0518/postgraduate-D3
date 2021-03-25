4778de8e104cb6076f81ba73af17713dd46f5d00
veritas0518 <76930817+veritas0518@users.noreply.github.com>
2021-03-25 10:26:56 +0800

    use C to save a group of students
    Caculate the failed students in contest

---
+++ 计算不及格人人数.c
@@ -0,0 +1,31 @@
+//#include<stdio.h>
+//struct stu {
+//	char num[10];
+//	char name[10];
+//	char sex;
+//	float score;
+//
+//}boy[] = { { "120321","zhangsan",'ann',45 },
+//{ "1221", "zhaan", 'ann', 62.5 },
+//{ "121", "zhsan", 'ann', 92.5},
+//{ "12321", "zhgan", 'ann', 87 },
+//	{ "12321", "zhgan", 'ann', 58 },
+//
+//};
+//int main() {
+//
+//	float ave;
+//	int count = 0;
+//	float sum = 0;
+//	for (int i = 0; i < 5; i++) {
+//		sum = sum + boy[i].score;
+//		if (boy[i].score < 60) {
+//			count++;
+//		}
+//	}
+//	ave = sum / 5.0;
+//	printf("%f\n", sum);
+//	printf("%f\n", ave);
+//	printf("%d", count);
+//	return 0;
+//}
